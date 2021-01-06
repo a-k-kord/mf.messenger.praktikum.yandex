ede10a345db5c49b2ee58a0102780538c217f577
Andrey Korotayev <a.k.kord@yandex.ru>
2021-01-06 17:58:11 +0300

    initial

---
+++ server.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+/**
+ * @File   : server.js
+ * @Author :  (a-k-kord)
+ * @Link   : 
+ * @Date   : 12/23/2020, 7:12:55 PM
+ */
+
+const express = require('express')
+
+const app = express()
+
+const PORT = 4000
+
+const API_PREFIX = '/api/v1';
+
+app.use(express.static(__dirname))
+
+app.listen(PORT, function() {
+    console.log(`Example app listening in port ${PORT}! ${__dirname}`)
+})

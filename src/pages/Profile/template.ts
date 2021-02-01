e03861c9647454542ff21cdb47da7c4d4e43c8ad
Andrey Korotayev <a.k.kord@yandex.ru>
2021-02-01 13:46:32 +0300

    hide not ready errors

--- src/pages/Profile/template.ts
+++ src/pages/Profile/template.ts
@@ -36,7 +36,7 @@ export default `
   <a href="#" class="popup__close">&times;</a>
   <form class="form form--columns uploaded box box--round-border--small box--has-shadow box--center popup__inner">
     <h1 class="form__title text text--align--center">Загрузите файл</h1>
-    <div class="uploaded__error text text--theme--danger form__error-msg">Ошибка, попробуйте ещё раз</div>
+    <div class="uploaded__error text text--theme--danger form__error-msg--hidden">Ошибка, попробуйте ещё раз</div>
     <div class="form__content">
       <div class="form__item">
         <label class="text text--size--small text--theme--primary text--align--center link--with--underline box--as--block box--width--150 box--margin--auto">
@@ -50,7 +50,7 @@ export default `
       <button
               class="text text--theme--light text--size--small text--weight--bold button button__text"  type="submit"  formmethod="POST">Поменять</button>
     </div>
-    <div class="text text--size--small text--theme--danger form__error-msg">Нужно выбрать файл</div>
+    <div class="text text--size--small text--theme--danger form__error-msg--hidden">Нужно выбрать файл</div>
   </form>
 </div>
 `

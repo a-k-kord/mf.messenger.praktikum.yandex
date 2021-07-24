0437f43ceb9a27b6a3559c9b505ed154690775bf
Andrey Korotayev <a.k.kord@yandex.ru>
2021-07-24 13:10:23 +0300

    return host = '0.0.0.0' to server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const express = require('express');
 
 const app = express();
 
-// const hostname = '0.0.0.0'; // сервер запустим на всех интерфейсах
+const hostname = '0.0.0.0'; // сервер запустим на всех интерфейсах
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/dist`));
@@ -20,7 +20,7 @@ app.get('/*', (req, res) => {
     res.sendFile(`${__dirname}/dist/index.html`);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, hostname, () => {
     // eslint-disable-next-line no-console
     console.log(`Example app listening in port ${PORT}! (in folder ${__dirname})`);
 });
